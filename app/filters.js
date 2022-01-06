10a1ff26cec17b2fe8524f97251d1d187fc3ed75
Ife Akinbolaji <ife.akinbolaji@hotmail.com>
2022-01-06 01:16:54 +0000

    test

---
+++ app/filters.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const path = require('path')
+const individualFiltersFolder = path.join(__dirname, './filters')
+
+module.exports = function (env) {
+  /**
+   * Instantiate object used to store the methods registered as a
+   * 'filter' (of the same name) within nunjucks. You can override
+   * gov.uk core filters by creating filter methods of the same name.
+   * @type {Object}
+   */
+  var filters = {}
+
+   // Import filters from filters folder
+   if (fs.existsSync(individualFiltersFolder)) {
+    var files = fs.readdirSync(individualFiltersFolder)
+    files.forEach(file => {
+      let fileData = require(path.join(individualFiltersFolder, file))
+      // Loop through each exported function in file (likely just one)
+      Object.keys(fileData).forEach((filterGroup) => {
+        // Get each method from the file
+        Object.keys(fileData[filterGroup]).forEach(filterName => {
+          filters[filterName] = fileData[filterGroup][filterName]
+        })
+      })
+    })
+  }
+
+  filters.toArray = (input) => {
+    return [].concat(input)
+  }
+
+  /* ------------------------------------------------------------------
+    add your methods to the filters obj below this comment block:
+    @example:
+
+    filters.sayHi = function(name) {
+        return 'Hi ' + name + '!'
+    }
+
+    Which in your templates would be used as:
+
+    {{ 'Paul' | sayHi }} => 'Hi Paul'
+
+    Notice the first argument of your filters method is whatever
+    gets 'piped' via '|' to the filter.
+
+    Filters can take additional arguments, for example:
+
+    filters.sayHi = function(name,tone) {
+      return (tone == 'formal' ? 'Greetings' : 'Hi') + ' ' + name + '!'
+    }
+
+    Which would be used like this:
+
+    {{ 'Joel' | sayHi('formal') }} => 'Greetings Joel!'
+    {{ 'Gemma' | sayHi }} => 'Hi Gemma!'
+
+    For more on filters and how to write them see the Nunjucks
+    documentation.
+
+  ------------------------------------------------------------------ */
+
+  /* ------------------------------------------------------------------
+    keep the following line to return your filters to the app
+  ------------------------------------------------------------------ */
+  return filters
+}
