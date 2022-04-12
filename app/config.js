c44edcb28c8e45e0382b5023baa5b1bab765c655
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-04-12 16:07:20 +0100

    update service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Early Years Foundation Recovery',
 
   // Default port that prototype runs on
   port: '3000',
