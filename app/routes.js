ba017b3b5d84f1f8f8f311f28ade9eb837b910f8
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-05-03 14:03:50 +0100

    add routes logic

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,16 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/quiz-1-answer', function (req, res) {
+
+  var partOfBrain = req.session.data['part-of-brain']
+
+  if (partOfBrain == "Occipital lobes"){
+    res.redirect('sprint-1/brain-development/quiz-1-right')
+  } else {
+    res.redirect('sprint-1/brain-development/quiz-1-wrong')
+  }
+
+})
+
 module.exports = router
