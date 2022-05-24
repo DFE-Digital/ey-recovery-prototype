0adfa3a5739f58f28bd835485d548e472ba5070f
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-05-24 16:26:27 +0100

    add and update pages for sprint 4

--- app/routes.js
+++ app/routes.js
@@ -13,6 +13,21 @@ router.post('/quiz-1-answer', function (req, res) {
     res.redirect('sprint-1/brain-development/quiz-1-wrong')
   }
 
-})
+});
+
+//routing for incorrect/correct quiz results
+router.post('/brain-development/test-result', (req, res) => {
+  // Make a variable and give it the value from 'quiz result value in radio buttons'
+  const quizResult = req.session.data['quiz-result'];
+
+  // Check whether the variable matches a condition
+  if (quizResult === 'correct') {
+    // Send user to next page
+    res.redirect('/brain-development/test-1-pass');
+  } else {
+    // Send user to ineligible page
+    res.redirect('/brain-development/test-1-fail');
+  }
+});
 
 module.exports = router
