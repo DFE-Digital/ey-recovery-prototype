26bae534a884231615ee10eb28281b712593f406
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-05-25 11:29:01 +0100

    update routes, add all pages from previous sprint

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { Console } = require('console')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
@@ -27,6 +28,23 @@ router.post('/brain-development/test-result', (req, res) => {
   } else {
     // Send user to ineligible page
     res.redirect('/brain-development/test-1-fail');
+  }
+});
+
+ //routing for quiz 1 validation
+ router.post('/brain-development/quiz-1-answer', (req, res) => {
+  // Make a variable and give it the value from 'quiz result value in radio buttons'
+  var quizAnswer = req.session.data['quiz-answer'];
+
+  // Check whether the variable matches a condition
+  if(quizAnswer === 'filled') {
+    //Radio button is checked
+
+    // Send user to next page
+    res.redirect('/brain-development/quiz-1-right');
+  } else {
+    // Send user to error page
+    res.redirect('/brain-development/quiz-1-error');
   }
 });
 
