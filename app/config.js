d31bf5f92f54a940699271e2752b991c6835d5b7
Becca Gorton <104020160+beccagorton-1@users.noreply.github.com>
2022-08-08 14:51:22 +0100

    Update service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Child development training',
+  serviceName: 'Early years child development training',
 
   // Default port that prototype runs on
   port: '3000',
